cc0fddc7996622e8fa94eabb5895a9cb9c2cd266
BahaaEldinZahi <hamadazahi699@gmail.com>
2025-07-01 03:29:40 +0300

    add CustomerUser model  from django with profile image field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class CustomerUser(AbstractUser):
+    image = models.ImageField(upload_to='profile_pics', default='default.jpg', blank=True)
+    def __str__(self):
+        return f"{self.username} ({self.email})"
