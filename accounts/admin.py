a25d398d630cceca70f42bc2b1e9e848bef429a6
BahaaEldinZahi <hamadazahi699@gmail.com>
2025-07-02 18:34:26 +0300

    feat: implement custom user admin interface with enhanced fieldsets

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
-from django.contrib.auth.admin import UserAdmin 
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login')
+    search_fields = ('username', 'email')
+    readonly_fields = ('date_joined', 'last_login')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Information', {'fields': ('email', 'image')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
