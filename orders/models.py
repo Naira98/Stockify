a7a89358140bb515a3df00274e6c3c679920d49f
Mohsen <Mohsen@gmail.com>
2025-07-07 04:08:13 +0200

    style: add newlines for better code readability in models.py

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Supermarket(models.Model):
@@ -10,5 +11,5 @@ class Supermarket(models.Model):
 
     def __str__(self):
         return self.name
-    
-    
+
+
