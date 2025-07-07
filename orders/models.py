b1719c39062cc4036f540d0403a44c7cf24deedd
Mohsen <Mohsen@gmail.com>
2025-07-07 03:51:59 +0200

    feat: add Supermarket model with name and location fields

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Supermarket(models.Model):
+    """Supermarket model for customers"""
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    # location = models.CharField(max_length=255, help_text="E.g. Supermarket branch name or city")
+
+    def __str__(self):
+        return self.name
+    
+    
