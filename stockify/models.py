f9ecd1ca77417a8d5d2f8e799b707966cb254e2f
Naira98 <nairamm99@gmail.com>
2025-07-09 22:50:57 +0300

    feat(script): update script to automate shipments and orders creation

--- stockify/models.py
+++ stockify/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 class TimestampModel(models.Model):
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
         ordering = ["-created_at"]
+
+    def save(self, *args, **kwargs):
+        if not kwargs.pop("manual_update", False):
+            self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
