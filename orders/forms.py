32e44b6b8c0d7312f952a94a0418cd2da21ff4d4
Mo7sen56 <mohmamedmosen41@gmail.com>
2025-07-09 17:23:53 +0200

    fix: add widgets to SupermarketForm for improved input styling

--- orders/forms.py
+++ orders/forms.py
@@ -8,3 +8,9 @@ class SupermarketForm(forms.ModelForm):
     class Meta:
         model = Supermarket
         fields = ["name", "location"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                }
+            ),
