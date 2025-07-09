c66c851276ae933e84a1b12fe85a9f2b3506aaf3
Mo7sen56 <mohmamedmosen41@gmail.com>
2025-07-09 17:21:15 +0200

    fix: add string representation method to OrderItem model

--- orders/models.py
+++ orders/models.py
@@ -146,3 +146,6 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
