f3bcdf5cbaabb3aad62d89c952421afe640ac491
Mo7sen56 <mohmamedmosen41@gmail.com>
2025-07-09 17:23:33 +0200

    fix: add SupermarketForm for managing Supermarket model data

--- orders/forms.py
+++ orders/forms.py
@@ -2,3 +2,9 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Submit
 from .models import Order, OrderItem, Supermarket
+
+
+class SupermarketForm(forms.ModelForm):
+    class Meta:
+        model = Supermarket
+        fields = ["name", "location"]
