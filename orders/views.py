2c567b577205bfe4e630ab59b45d336df95f0cf6
Mo7sen56 <mohamedmosen41@gmail.com>
2025-07-10 15:43:50 +0200

    fix: reorganize imports in views.py for better readability and structure

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Q
+from django.views import View
+# from .decorators import manager_required
+from django.db import transaction
+from .models import Order, OrderItem, Supermarket
+from collections import defaultdict
+from inventory.models import Product
+from django.db.models import Count
+from .forms import OrderForm, OrderItemForm
+from .forms import SupermarketForm
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, DeleteView, ListView, DetailView
+from django.forms import inlineformset_factory
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 
