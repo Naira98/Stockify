2c46fc7574b0b9ba2f3a06f8ab35501c12adbc4d
Mo7sen56 <mohamedmosen41@gmail.com>
2025-07-10 15:49:26 +0200

    fix: reorganize import statements in urls.py for improved structure

--- orders/urls.py
+++ orders/urls.py
@@ -1,4 +1,21 @@
 from django.urls import path
 from accounts.models import User
-
+from .views import (
+    OrderListView,
+    create_order,
+    SupermarketCreateView,
+    SupermarketListView,
+    DeleteSupermarketView,
+    OrderDeleteView,
+    DeleteProductFromOrderView,
+    AddProductToOrderView,
+    EditProductInOrderView,
+    ChangeOrderStatusView,
+    ConfirmOrderView,
+)
 app_name = 'orders'
+
+
+urlpatterns = [
+     path("", OrderListView.as_view(), name="order_list"),
+]
